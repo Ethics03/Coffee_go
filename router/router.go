8f44ff9e7d669d5a55e2d00af08f6761dc105c14
Rachit Srivastava <honey032004@gmail.com>
2024-12-01 02:51:12 +0530

    feat: Added routers

---
+++ router/router.go
@@ -0,0 +1,31 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/Ethics03/basic/cmd/controllers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func Routes() http.Handler {
+	router := chi.NewRouter()
+	router.Use(middleware.Recoverer)
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	router.Get("/api/v1/coffees", controllers.GetAllCoffees)
+	router.Post("/api/v1/coffees/coffee", controllers.CreateCoffee)
+
+	return router
+
+}
